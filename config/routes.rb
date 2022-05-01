7febdf42b5b19ead1b28f009478bad40a5a8cc83
Deby2404 <dedede2404@gmail.com>
2022-05-01 08:25:40 +0000

    make my_page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+
   root to: 'homes#top'
+
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
   }
@@ -7,5 +9,12 @@ Rails.application.routes.draw do
     registrations:"public/registrations",
     sessions:"public/sessions"
   }
+
+  namespace :public do
+    get "/customers/my_page" => "customers#show" #会員情報詳細ページ（マイページ）表示
+    resources :customers, only:[:edit,:update]
+  end
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
