7a6ef61c94d06de79184d5797db5d946e37a94d4
Deby2404 <dedede2404@gmail.com>
2022-05-05 06:59:26 +0000

    make ユーザー投稿詳細画面

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,9 +1,11 @@
 class Admin::CustomersController < ApplicationController
   def index
-    @customers = Customer.all
+    @customers = Customer.page(params[:page])
   end
 
   def show
+    @customer = Customer.find(params[:id])
+    @menus = @customer.menus
   end
 
   def edit
