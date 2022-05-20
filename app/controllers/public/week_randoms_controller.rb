2f30ab76bb62cfa74b58ba9e8717121eaf9ca573
Deby2404 <dedede2404@gmail.com>
2022-05-20 11:10:53 +0000

    [add]error_message in day_randoms

--- app/controllers/public/week_randoms_controller.rb
+++ app/controllers/public/week_randoms_controller.rb
@@ -9,13 +9,11 @@ class Public::WeekRandomsController < ApplicationController
     params[:menus].each{ |key, values|
       @menus[key] = Menu.order("RANDOM()").where(genre_id: values[:genre_id]).where(food_status: values[:food_status]).limit(1)
     }
-    render :show
-    if @menus.present?
-      redirect_to public_week_random_path(@menus.ids) #詳細ページに遷移
+    if @menus.blank?
+      flash[:danger] = ["食事もしくはジャンルが選択されていません"]
+      redirect_to request.referer
     else
-        @menu = Menu.all #menuの情報全部取得
-        @genre = Genre.all #genreの情報全部取得
-        render :new
+      redirect_to public_week_random_path(@menus.ids) #詳細ページに遷移
     end
   end
 
