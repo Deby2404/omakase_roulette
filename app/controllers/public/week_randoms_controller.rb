039e0e9002bb0f54710736caa0609ff1cdf3045f
Deby2404 <dedede2404@gmail.com>
2022-05-21 07:56:02 +0000

    [add]action_mailer in production.rb

--- app/controllers/public/week_randoms_controller.rb
+++ app/controllers/public/week_randoms_controller.rb
@@ -1,29 +1,30 @@
 class Public::WeekRandomsController < ApplicationController
   def new
-    @menu = Menu.all #menuの情報全部取得
-    @genre = Genre.all #genreの情報全部取得
+    @menus = Menu.all #menuの情報全部取得
+    @genres = Genre.all #genreの情報全部取得
   end
 
   def create
-    @menus ={}
+    @menu ={}
     params[:menus].each{ |key, values|
-      @menus[key] = Menu.order("RANDOM()").where(genre_id: values[:genre_id]).where(food_status: values[:food_status]).limit(1)
+      @menu[key] = Menu.order("RANDOM()").where(genre_id: values[:genre_id]).where(food_status: values[:food_status]).limit(1)
     }
-    if @menus.blank?
-      flash[:danger] = ["食事もしくはジャンルが選択されていません"]
-      redirect_to request.referer
+    if @menu.present?
+      redirect_to public_week_random_path(@menu.id) #詳細ページに遷移
     else
-      redirect_to public_week_random_path(@menus.ids) #詳細ページに遷移
+        @menus = Menu.all #menuの情報全部取得
+        @genres = Genre.all #genreの情報全部取得
+        render :new
     end
   end
 
   def show
-    @menus = Menu.find(params[:id])
-    @genre = @menus.genre
+    @menu = Menu.find(params[:id])
+    @genres = @menu.genre
   end
 
   private
-  def menu_params
+  def random_params
     params.require(:menu).permit(:food_status, :genre_id)
   end
 end
